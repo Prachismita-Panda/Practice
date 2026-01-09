bdef4b41ba24a14dbf62909cde0dfca21b559050
unknown <prachismitapanda10@gmail.com>
2026-01-09 22:04:23 +0530

    content added

---
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/StoreContext'
+
+
+const FoodDisplay = () => {
+
+    const {food_list} = useContext(StoreContext)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FoodDisplay
