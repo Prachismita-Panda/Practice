8173545bb8d3c645b5002f35874257db232ad122
unknown <prachismitapanda10@gmail.com>
2026-01-13 22:48:33 +0530

    content added

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -15,7 +15,10 @@ const ExploreMenu = ({category,setCategory}) => {
                     <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
                         <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                         <p>{item.menu_name}</p>
+                        <h2></h2>
                     </div>
+
+                    
                 )
             })}
 
